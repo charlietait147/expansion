e67465659e01bb7c1ac3035d8fb53b342170b092
Charlie Tait <charlietait147@gmail.com>
2023-12-01 23:08:25 +0000

    add desktop styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Logo from "../public/images/expansion-logo.png";
 function Header() {
     return ( 
-        <header className="flex justify-between pb-2 pl-4 md:pl-8">
+        <header className="flex justify-between pb-2 pl-4 md:pl-8 lg:max-w-screen-lg m-auto">
             <h1 className=" text-2xl font-semibold text-title pt-4 tracking-wider md:text-3xl">expansion</h1>
             <Image src = {Logo} alt="expansion logo" className="w-20 md:w-24" />
         </header>
